ba33e2d64b9a8c9ae3ebf34076e16ae8fcedaead
massa <massafranck@gmail.com>
2018-09-10 14:16:30 +0200

    navbar

--- index.php
+++ index.php
@@ -3,15 +3,21 @@
 <html lang="fr">
   <head>
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
     <link rel="stylesheet" href="assets/style.css" />
     <title>Maçonnerie Ocordo</title>
   </head>
   <body>
     <div class="container-fluid">
+        <nav>
+    <div class="nav-wrapper">
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
     <?php for ($i=0; $i < $sizeTab ; $i++) { ?>
-      <a href="<?php echo $xml->page[$i]['id'] ?>"><?php echo $xml->page[$i]->menu ?></a>
+        <li><a href="<?php echo $xml->page[$i]['id'] ?>"><?php echo $xml->page[$i]->menu ?></a></li>
     <?php } ?>
+      </ul>
+    </div>
+  </nav>
       <p><?php echo $xml->page[0]->content; ?></p>
     </div>
   </body>
