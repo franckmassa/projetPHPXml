6ee84cb4ee59da097f37bffa71c34713144a48d5
massa <massafranck@gmail.com>
2019-04-06 11:45:02 +0200

    maj netbeans

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
         <link rel="stylesheet" href="assets/css/style.css" />
         <title><?= $xml->page[$pageRef - 1]->title ?></title>
     </head>
+
     <body>
         <div class="container-fluid w-75 rounded">
             <nav class="navbar navbar-expand-lg navbar-light fixed-top">
@@ -29,20 +30,21 @@
             <!-- Permet d'afficher le noeud(balise) content en fonction de la valeur de la variable $pageRef -->
             <!-- La variable $pageRef contient la valeur du paramètre page du GET -->
             <p><?= $xml->page[$pageRef - 1]->content ?></p>
-        <!-- on vérifie que les champs existent et qu'ils sont valide sinon on affiche les messages d'erreur -->
-        <p class="text-danger text-center font-weight-bold"> <?= isset($formError['your-name']) ? $formError['your-name'] : ' '; ?></p>
-        <p class="text-danger text-center font-weight-bold"> <?= isset($formError['your-email']) ? $formError['your-email'] : ' '; ?></p>
-        <p class="text-danger text-center font-weight-bold"> <?= isset($formError['your-tel-615']) ? $formError['your-tel-615'] : ' '; ?></p>
-        <p class="text-danger text-center font-weight-bold"> <?= isset($formError['your-subject']) ? $formError['your-subject'] : ' '; ?></p>
-        <p class="text-danger text-center font-weight-bold"> <?= isset($formError['your-ville']) ? $formError['your-ville'] : ' '; ?></p>
-        <p class="text-danger text-center font-weight-bold"> <?= isset($formError['your-message']) ? $formError['your-message'] : ' '; ?></p>
-        <p class="text-success text-center font-weight-bold"> <?= isset($_POST['send']) && count($formError) == 0 ? 'Message envoyé !' : ' '; ?></p>
-      </div>
-                <footer>
-                    <div class="footer-copyright text-center py-3">© 2018 Copyright - Maçonnerie Ocardo</div>
-                </footer>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" ></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" ></script>
+            <!-- on vérifie que les champs existent et qu'ils sont valide sinon on affiche les messages d'erreur -->
+            <p class="text-danger text-center font-weight-bold"> <?= isset($formError['your-name']) ? $formError['your-name'] : ' '; ?></p>
+            <p class="text-danger text-center font-weight-bold"> <?= isset($formError['your-email']) ? $formError['your-email'] : ' '; ?></p>
+            <p class="text-danger text-center font-weight-bold"> <?= isset($formError['your-tel-615']) ? $formError['your-tel-615'] : ' '; ?></p>
+            <p class="text-danger text-center font-weight-bold"> <?= isset($formError['your-subject']) ? $formError['your-subject'] : ' '; ?></p>
+            <p class="text-danger text-center font-weight-bold"> <?= isset($formError['your-ville']) ? $formError['your-ville'] : ' '; ?></p>
+            <p class="text-danger text-center font-weight-bold"> <?= isset($formError['your-message']) ? $formError['your-message'] : ' '; ?></p>
+            <p class="text-success text-center font-weight-bold"> <?= isset($_POST['send']) && count($formError) == 0 ? 'Message envoyé !' : ' '; ?></p>
+        </div>
+        <footer>
+            <div class="footer-copyright text-center py-3">© 2018 Copyright - Maçonnerie Ocardo</div>
+        </footer>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" ></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" ></script>
     </body>
+
 </html>
